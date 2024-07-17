8721c12e3297205d347b782564bd3134328a65c3
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-17 18:26:54 -0300

    Building Error: Export encountered error on paths...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,18 @@
-'use client';
-
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import LocalSwitcher from './local-switcher';
 import { raleway } from '@/ui/font';
 
-interface HeaderProps {
-  locale: string;
-}
 
-export default function Header({ locale }: HeaderProps) {
+export default function Header() {
   const t = useTranslations('Navigation');
-  const pathname = usePathname();
-
-  const buildLocalizedUrl = (path: string) => {
-    return `/${locale}${path}`;
-  };
 
   return (
     <header className={`${raleway.className} text-cyan-400 my-8 border-b-2 p-2 flex items-center justify-between`}>
       <nav className='flex gap-4 font-bold text-xl'>
-        <Link href={buildLocalizedUrl('/')}>{t('home')}</Link>
-        <Link href={buildLocalizedUrl('/about')}>{t('about')}</Link>
-        <Link href={buildLocalizedUrl('/contact')}>{t('contact')}</Link>
+        <Link href={('/')}>{t('home')}</Link>
+        <Link href={('/about')}>{t('about')}</Link>
+        <Link href={('/contact')}>{t('contact')}</Link>
       </nav>
       <LocalSwitcher />
     </header>
--- src/components/local-switcher.tsx
+++ src/components/local-switcher.tsx
@@ -1,44 +1,31 @@
 'use client';
 
 import { useLocale } from 'next-intl';
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { ChangeEvent, useTransition } from 'react';
 
 export default function LocalSwitcher() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const localActive = useLocale();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const nextLocale = e.target.value;
-
-    const newUrl = new URL(window.location.href);
-    newUrl.pathname = newUrl.pathname.replace(`/${localActive}`, `/${nextLocale}`);
-
-    if (searchParams) {
-      newUrl.search = searchParams.toString();
-    }
-
     startTransition(() => {
-      router.replace(newUrl.toString());
+      router.replace(`/${nextLocale}`);
     });
   };
-
   return (
-    <label className='border-none text-black'>
-      <p className='sr-only'>Change Language</p>
+    <label className='border-2 rounded'>
+      <p className='sr-only'>change language</p>
       <select
         defaultValue={localActive}
-        className='font-medium rounded p-1'
-        name=""
-        id=""
+        className='bg-transparent py-2'
         onChange={onSelectChange}
         disabled={isPending}
       >
-        <option value="en">English</option>
-        <option value="es">Spanish</option>
+        <option value='en'>English</option>
+        <option value='es'>Spanish</option>
       </select>
     </label>
   );
