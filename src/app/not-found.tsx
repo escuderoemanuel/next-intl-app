7c8287aa91bbdf4610243097d8e545e7cd16c673
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-17 17:03:31 -0300

    Test Branch

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <body>
         <div className='text-white flex flex-col items-center justify-center h-screen'>
           <h1 className='text-3xl'>Page Not Found!</h1>
-          <h2  className='text-xl'>Something went wrong...</h2>
+          <h2  className='text-xl'>Something went wrong 🚧</h2>
         </div>
       </body>
     </html>
